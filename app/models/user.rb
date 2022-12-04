3bbd96fccdfb971a8bfcabc3f2a827c94a71412e
Aleksandr Shpynev <arch.shpynev@gmail.com>
2022-12-04 23:08:27 +0300

    add regexp for mail and name, small style fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
 
+  VALID_NAME_REGEXP = /\A[A-Z](?=.{1,29}$)[A-Za-z]*(?:\h+[A-Z][A-Za-z]*)*$+\z/
+  VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
 
-  validates :first_name, :last_name, presence: true, length: { minimum: 2 }
-  validates :email, presence: true, uniqueness: true, format: { with: /@/ }
+  validates :first_name, :last_name, presence: true, length: { minimum: 2 }, format: { with: VALID_NAME_REGEXP }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEXP }
 end
