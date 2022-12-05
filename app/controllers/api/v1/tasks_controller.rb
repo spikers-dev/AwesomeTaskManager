750ace966bc22b631f9f3177459766df30dbf6b2
Aleksandr Shpynev <arch.shpynev@gmail.com>
2022-12-05 23:47:04 +0300

    add task api and test

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::TasksController < Api::V1::ApplicationController
+  def index
+    tasks = Task.ransack(ransack_params).
+      result.
+      page(page).
+      per(per_page)
+
+    respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+  end
+
+  def show
+    respond_with(task, serializer: TaskSerializer)
+  end
+
+  def create
+    task = current_user.my_tasks.new(task_params)
+    task.save
+
+    respond_with(task, serializer: TaskSerializer, location: nil)
+  end
+
+  def update
+    task.update(task_params)
+
+    respond_with(task, serializer: TaskSerializer)
+  end
+
+  def destroy
+    task.destroy
+
+    respond_with(task)
+  end
+
+  private
+
+  def task
+    @task ||= Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:author_id, :assignee_id, :description, :expired_at, :name, :state_event)
+  end
+end
