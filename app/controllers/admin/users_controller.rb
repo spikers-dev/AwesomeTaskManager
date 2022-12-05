b9683b1c52a0da1e32297ee8149b74284955280c
Aleksandr Shpynev <arch.shpynev@gmail.com>
2022-12-05 21:57:49 +0300

    refactoring code, add seeds

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,47 @@
 class Admin::UsersController < Admin::ApplicationController
   def show
-    @user = User.find(params[:id])
+    user
   end
 
   def index
     @q = User.ransack(params[:q])
-    @users = @q.result.order(:id).order(:type).page(params[:page]).per(params[:per_page])
+    @users = @q.result.order(:id).order(:type).page(params[:page]).per(params[:per_page]).per(10)
+  end
+
+  def edit
+    user
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    return redirect_to(admin_users_url) if @user.save
+
+    render(:new)
+  end
+
+  def update
+    return redirect_to(admin_users_url) if user.update(user_params)
+
+    render(:edit)
+  end
+
+  def destroy
+    user.destroy
+    redirect_to(admin_users_path)
+  end
+
+  private
+
+  def user
+    @user ||= User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :type)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+admin = Admin.find_or_create_by(first_name: 'Admin', last_name: 'Adminovich', email: 'admin@mail.com')
+admin.password = 'default'
+admin.save
+
+developer = Developer.find_or_create_by(first_name: 'Developer', last_name: 'Dev', email: 'developer@mail.com')
+developer.password = 'default'
+developer.save
+
+manager = Manager.find_or_create_by(first_name: 'Manager', last_name: 'Man', email: 'manager@mail.com')
+manager.password = 'default'
+manager.save
+
+60.times do
+  u = [Manager, Developer].sample.new
+  u.first_name = Faker::Name.first_name
+  u.last_name = Faker::Name.last_name
+  u.email = Faker::Internet.email(name: "#{u.first_name} #{u.last_name}", separators: '.')
+  u.password = 'default'
+  u.save
+end
+
+70.times do
+    t = Task.new
+    t.name = Faker::Hacker.noun.capitalize
+    t.description = Faker::Hacker.say_something_smart
+    t.author = User.where(type: "Manager").sample
+    t.assignee = User.where(type: "Developer").sample
+    t.state = ['new_task',
+               'in_development',
+               'in_qa',
+               'in_code_review',
+               'ready_for_release',
+               'released',
+               'archived',
+              ].sample
+    t.expired_at = Faker::Time.between(from: DateTime.now - 1, to: DateTime.now + 30)
+    t.save
+  end
