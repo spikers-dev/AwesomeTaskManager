b3369d7908023db6730403c9f0856e26c09a9f49
Aleksandr Shpynev <arch.shpynev@gmail.com>
2022-12-10 21:40:36 +0300

    add eslint and prettier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "material-design-lite/material.js"
+import Rails from '@rails/ujs';
+import * as ActiveStorage from '@rails/activestorage';
+import 'channels';
+import 'material-design-lite/material.js';
 
-Rails.start()
-ActiveStorage.start()
+Rails.start();
+ActiveStorage.start();
