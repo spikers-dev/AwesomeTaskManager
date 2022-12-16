f6b0ecc7ab63237dafb7fbec606dadd62e7a7903
Aleksandr Shpynev <arch.shpynev@gmail.com>
2022-12-16 22:07:21 +0300

    add action to tasks

--- app/javascript/components/EditPopup/components/Form/useStyles.js
+++ app/javascript/components/EditPopup/components/Form/useStyles.js
@@ -2,7 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
   root: {
-    width: '10vw',
+    display: 'flex',
+    flexDirection: 'column',
   },
 }));
 
